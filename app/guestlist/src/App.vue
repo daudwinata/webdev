4740f585eee5f620f3c428c255ba0fdbf383ef6b
Daud Winata <daud.winata@yahoo.ca>
2020-01-13 09:56:12 -0500

    add guestlist file

---
+++ app/guestlist/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="main">
+    <Header></Header>
+    <h1>{{title}}</h1>
+    <Body></Body>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Body from "./components/Body.vue";
+
+export default {
+  data: function() {
+    return {
+      title: "Guest List"
+    };
+  },
+  components: {
+    Header,
+    Body
+  }
+};
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.main h1 {
+  text-align: center;
+  margin-top: 40px;
+}
+</style>
