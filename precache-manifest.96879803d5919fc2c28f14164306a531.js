c54100943468469b21dfc43fa0b652d5c47b0a34
Daud Winata <daud.winata@yahoo.ca>
2020-10-15 01:06:13 -0400

    Updates

---
+++ precache-manifest.96879803d5919fc2c28f14164306a531.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b532f52709646a3323ba4f4304844f16",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "2d5566a5ff6aef53aa62",
+    "url": "/demo/static/css/2.ddd0df21.chunk.css"
+  },
+  {
+    "revision": "43b8f75f7960a9428b9a",
+    "url": "/demo/static/css/main.6b7f6a60.chunk.css"
+  },
+  {
+    "revision": "2d5566a5ff6aef53aa62",
+    "url": "/demo/static/js/2.ff26fa65.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo/static/js/2.ff26fa65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b8f75f7960a9428b9a",
+    "url": "/demo/static/js/main.eb9413b5.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  },
+  {
+    "revision": "bd526d03e320973baddf13b0aaf35d17",
+    "url": "/demo/static/media/ADT_website_Business_Video-surveillance.bd526d03.jpg"
+  },
+  {
+    "revision": "e3606a9be2551855c9239acf48c5e3e1",
+    "url": "/demo/static/media/business.e3606a9b.jpg"
+  },
+  {
+    "revision": "1dae6bfa412695a236d2d8a006e6351d",
+    "url": "/demo/static/media/generation-panel.1dae6bfa.png"
+  },
+  {
+    "revision": "d7479e90c5bdd2dc343cc08b9f1a3c3f",
+    "url": "/demo/static/media/logo.d7479e90.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/demo/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/demo/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/demo/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/demo/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "3d05b1513896d1989daed8e3009a7930",
+    "url": "/demo/static/media/smart-smoke-detector.3d05b151.png"
+  },
+  {
+    "revision": "eb7c30d248f4a24e34b8a007532bdf63",
+    "url": "/demo/static/media/the_best_of_both_worlds.eb7c30d2.png"
+  }
+]);
